6b830a00d70842ae4c54cc1d0de657bf70bf110e
DoloresPGz <dolores.pgz@gmail.com>
2024-04-23 19:38:10 -0600

    Base de la lista enlazada

---
+++ 3-1-1-Listas-Enlazadas/lista-enlazada.js
@@ -0,0 +1,20 @@
+class Node{
+     constructor(data){
+        this.data = data //La información que vamos a almacenar
+        this.next = null //Puntero o enlace que me va a ayudar a señalar el siguiente dato de la lista enlazada
+     }
+}
+
+class LinkedList{
+    constructor(head = null){
+        this.head = head
+    }
+
+    //Insertar un nodo la lista
+
+    //Borrar un elemento de la estructura
+
+    //Recorrido de la lista
+
+    //Búsqueda de un elemento
+}

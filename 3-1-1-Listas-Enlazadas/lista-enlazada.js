911add09a91fcc25cdb2ce260ee1accca1bcd5be
DoloresPGz <dolores.pgz@gmail.com>
2024-04-23 20:10:39 -0600

    método de insert funcional

--- 3-1-1-Listas-Enlazadas/lista-enlazada.js
+++ 3-1-1-Listas-Enlazadas/lista-enlazada.js
@@ -11,6 +11,26 @@ class LinkedList{
     }
 
     //Insertar un nodo la lista
+    insert(data){
+       //Crear un nuevo nodo que se va a enlazar a mi estructura
+       let node = new Node(data)
+
+       //Al agrear un nuevo nodo a la lista, puedo encontrarme con dos casos
+       //Primer caso: La lista enlazada se encuentra vacía (head ->null )
+       if(this.head == null){
+            //Agrego el nodo al inicio de la lista (head -> node)
+            this.head = node
+       }else{
+        //Segundo caso: Tengo por lo menos un nodo en la lista enlazada
+            let current = this.head //Mi punto de inicio para recorrer la lista enlazada
+            while(current.next != null){ //Verifico si el enlace del nodo en el que me encuentro apunta a nulo
+                current = current.next
+            }
+
+            //Cuando salgo del while sé que current se encuentra en el último nodo de mi lista
+            current.next = node
+       }
+    }
 
     //Borrar un elemento de la estructura
 
@@ -18,3 +38,12 @@ class LinkedList{
 
     //Búsqueda de un elemento
 }
+
+
+let ll = new LinkedList()
+
+ll.insert(1)
+ll.insert(2)
+ll.insert(3)
+
+console.log(ll);
